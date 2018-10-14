811ff6f29702689dcf2bbfa1fecc08ad46797a17
oshi192 <yuriykoss@gmail.com>
2018-10-14 19:29:54 +0300

    added gitignore

---
+++ src/main/java/model/entity/Contacts.java
@@ -0,0 +1,60 @@
+package model.entity;
+
+public class Contacts {
+    private String housePhoneNumber;
+    private String cellPhoneNumber;
+    private String secondCellPhoneNumber;
+    private String email;
+    private String skype;
+
+    private Contacts(Builder builder) {
+        this.housePhoneNumber = builder.housePhoneNumber;
+        this.cellPhoneNumber = builder.cellPhoneNumber;
+        this.secondCellPhoneNumber = builder.secondCellPhoneNumber;
+        this.email = builder.email;
+        this.skype = builder.skype;
+    }
+
+    public static class Builder {
+        private String housePhoneNumber;
+        private String cellPhoneNumber;
+        private String secondCellPhoneNumber;
+        private String email;
+        private String skype;
+
+        public Builder housePhoneNumber(String value) {
+            this.housePhoneNumber = value;
+            return this;
+        }
+
+        public Builder cellPhoneNumber(String value) {
+            this.cellPhoneNumber = value;
+            return this;
+        }
+
+        public Builder secondCellPhoneNumber(String value) {
+            this.secondCellPhoneNumber = value;
+            return this;
+        }
+
+        public Builder email(String value) {
+            this.email = value;
+            return this;
+        }
+
+        public Builder skype(String value) {
+            this.skype = value;
+            return this;
+        }
+
+        public Contacts build() {
+            return new Contacts(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
