6186403a41911d89814c0d1df062d13b794ef77f
oshi192 <yuriykoss@gmail.com>
2018-11-02 15:41:36 +0200

    create web branch and add servlet lib

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,11 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-        Connection connection = DriverManager. getConnection("jdbc:mysql://localhost/cursor1?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","root");
-        PreparedStatement statement = connection.prepareStatement("select * from cars");
-        ResultSet resultSet =  statement.executeQuery();
-        if(resultSet.next())System.out.println(resultSet.getString(2));
+//        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+//        Connection connection = DriverManager. getConnection("jdbc:mysql://localhost/cursor1?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","root");
+//        PreparedStatement statement = connection.prepareStatement("select * from cars");
+//        ResultSet resultSet =  statement.executeQuery();
+//        if(resultSet.next())System.out.println(resultSet.getString(2));
         View view = new View();
         Model model = new Model();
         Controller controller = new Controller(model, view);
---
+++ src/main/java/web/Main.java
@@ -0,0 +1,14 @@
+package web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Main extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
+
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
