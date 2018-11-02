e5c47cd855431c6c326ec4d4816ec8d331731e07
oshi192 <yuriykoss@gmail.com>
2018-11-02 16:24:22 +0200

    added register form and save method

--- src/main/java/web/Main.java
+++ src/main/java/web/Main.java
@@ -1,14 +1,77 @@
 package web;
 
+import controller.Regulars;
+import model.Model;
+import model.entity.Address;
+import model.entity.Contacts;
+import model.entity.Names;
+import model.exceptions.NotUniqueLoginException;
+
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/MyForm")
 public class Main extends HttpServlet {
-    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+    static Model model=new Model();
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
+        model.createNewEntity();
+        setNamesParameterToModel(request);
+        setAddressParameterToModel(request);
+        setContactsParameterToModel(request);
+        model.getTmpEntity().setComment(request.getParameter("comment"));
+        model.getTmpEntity().setNickName(request.getParameter("nickName"));
+        try {
+            model.saveEntity();
+        } catch (NotUniqueLoginException e) {
+            e.printStackTrace();
+        }
 
-        httpServletResponse.getWriter().print("Hello from servlet");
+        response.setContentType("text/html");
+        PrintWriter pw=response.getWriter();
+        pw.write("<h2> added <h2> <br>");
+
+    }
+    private void setNamesParameterToModel(HttpServletRequest request) {
+        model.getTmpEntity().setNames(
+                new Names.Builder()
+                        .name(request.getParameter("name"))
+                        .surname(request.getParameter("surname"))
+                        .secondName(request.getParameter("secondName"))
+                        .build()
+        );
+    }
+
+    /**
+     * fill fields "Contacts" field in Entity
+     */
+    private void setContactsParameterToModel(HttpServletRequest request) {
+        model.getTmpEntity().setContacts(
+                new Contacts.Builder()
+                        .housePhoneNumber(request.getParameter("housePhoneNumber"))
+                        .cellPhoneNumber(request.getParameter("cellPhoneNumber"))
+                        .secondCellPhoneNumber(request.getParameter("secondCellPhoneNumber"))
+                        .email(request.getParameter("email"))
+                        .skype(request.getParameter("skype"))
+                        .build()
+        );
+    }
+
+    /**
+     * fill fields "Address" field in Entity
+     */
+    private void setAddressParameterToModel(HttpServletRequest request) {
+        model.getTmpEntity().setAdress(
+                new Address.Builder().zip(request.getParameter("zip"))
+                        .city(request.getParameter("city"))
+                        .street(request.getParameter("street"))
+                        .houseNumber(request.getParameter("houseNumber"))
+                        .flatNumber(request.getParameter("flatNumber"))
+                        .build()
+        );
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,9 +1,36 @@
+<!DOCTYPE html>
 <html>
-    <body>
-        <h2>
-            Hello World!
-        </h2>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
 
-        <a href="servlet">Click here to see servlet</a>
-    </body>
+    Welcome to servlet 3 Form example
+    <br>
+    <br>
+    <form action="MyForm">
+    <h3>Names</h3>
+        Name: <input type="text" name="name"><br>
+        Surname: <input type="text" name="surname"><br>
+        Second name: <input type="text" name="secondName"><br>
+    <h3>Address</h3>
+        City: <input type="text" name="city"><br>
+        Street : <input type="text" name="street"><br>
+        House number : <input type="text" name="houseNumber"><br>
+        Flat number : <input type="text" name="flatNumber"><br>
+        Zip code: <input type="text" name="zip"><br>
+    <h3>Contacts</h3>
+        House phone number: <input type="text" name="housePhoneNumber"><br>
+        Cell phone number: <input type="text" name="cellPhoneNumber"><br>
+        Second cell phone number (optional) : <input type="text" name="secondCellPhoneNumber"><br>
+        Email: <input type="email" name="email"><br>
+        Skype: <input type="text" name="skype"><br>
+    <h3>Other</h3>
+        Login: <input type="text" name="nickName"><br>
+        Comment: <input type="text" name="comment"><br>
+        <input type="submit" value="Submit"> <input type="reset">
+    </form>
+
+</body>
 </html>
